6a0e0b990d06f3960fdb98db34ada879481dbfd2
tridibesh321 <114847107+tridibesh321@users.noreply.github.com>
2022-10-02 11:24:02 +0600

    Update magic_8ball_list.py

--- magic_8ball_list.py
+++ magic_8ball_list.py
@@ -1,3 +1,3 @@
 import random 
-messages=[ "It is certain ","It's deadly so","Are you crazy!?","Yes it is "]
+messages=[ "It is certain ","It's deadly so","Are you crazy!?","Yes it is ","Are you a psychopath"]
 print(messages[random.randint(0, len (messages))])
